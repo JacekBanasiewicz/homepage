9ec338d552f27524abd64af54885955d5e2b4eda
Jacek Banasiewicz <jacek.banasiewicz6@gmail.com>
2019-07-28 19:38:00 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+console.log('siema');
+
+const name = 'Jacek Banasiewicz';
+const age = 20;
+
+console.log(name);
+console.log(age);
+
+console.log(`Siema mam
+na imię ${name}
+i mam ${age} lat`);
+
+console.log('Jacek Banasiewicz mówi HEJ!😋');
