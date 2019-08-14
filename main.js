a831ce800e78ccbcfec9759b5934674519046a19
Jacek Banasiewicz <jacek.banasiewicz6@gmail.com>
2019-08-15 00:38:53 +0200

    dodane JSkowych ćwiczeń do console.log i zmiana koloru czcionki paragraphs

--- main.js
+++ main.js
@@ -11,3 +11,16 @@ na imię ${name}
 i mam ${age} lat`);
 
 console.log('Jacek Banasiewicz mówi HEJ!😋');
+
+
+const about = document.querySelector('.about__paragraph--js');
+
+console.log(about.innerHTML);
+
+about.innerHTML = `<strong>Siema</strong> mam na imię ${name} i mam ${age} lat`;
+
+const paragraphs = document.querySelectorAll('p');
+
+console.log(paragraphs[3]);
+
+
